ce70d1d2d0dc8037494e75aeef14df4dc89457a8
Serge Goussev <goussev.serge@gmail.com>
2025-09-30 13:36:42 -0400

    add a post processing step and add a conda env

---
+++ post-processing.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+
+def update_index():
+    """
+    Update the main index.html - which is the home page of the catalogue:
+    - Make the search on datasets
+    - Fix the reference to the catalogue home (top left)
+    - Change datacontract.com reference to reproducibility project site
+    """
+    
+    # Define the path to your HTML file
+    file_path = '_site/index.html'
+
+    # Open and read the HTML file
+    with open(file_path, 'r') as file:
+        html_content = file.read()
+
+    # Create a BeautifulSoup object to parse the content
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Change the main text
+    h2_tag_main_title = soup.find('h2', class_="text-2xl")
+    h2_tag_main_title.string = '\n              Price Statistics Open Data Catalogue'
+
+    # Modify the Catalogue home link
+    a_tag_top_left = soup.find('a', class_="text-xl text-gray-900 font-semibold")
+    a_tag_top_left.string = '\n              Catalogue Home\n            '
+    a_tag_top_left['href'] = "https://un-task-team-for-scanner-data.github.io/price-stats-data-catalogue/"
+    a_tag_top_left
+
+    # Modify the reference to the project
+    a_tag_top_right = soup.find('a', class_="text-sky-500 hover:text-gray-700 text-sm font-semibold")
+    # <a class="text-sky-500 hover:text-gray-700 text-sm font-semibold" href="https://datacontract.com" target="_blank">datacontract.com</a>
+    a_tag_top_right.string = 'Price Statistics Reproducibility Project'
+    a_tag_top_right['href'] = "https://un-task-team-for-scanner-data.github.io/reproducibility-project/docs/"
+
+    # Save the modified HTML back to the file
+    with open(file_path, 'w') as file:
+        file.write(str(soup))
+
+
+if __name__ == "__main__":
+    update_index()
