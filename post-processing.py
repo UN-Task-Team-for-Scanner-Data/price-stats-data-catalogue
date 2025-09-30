71ef6c4fb7ccb689751389a62405bf788e7aebff
Serge Goussev <goussev.serge@gmail.com>
2025-09-30 15:30:09 -0400

    fix post-processing.py

--- post-processing.py
+++ post-processing.py
@@ -38,6 +38,39 @@ def update_index():
     with open(file_path, 'w') as file:
         file.write(str(soup))
 
+def update_dataset(_path):
+    """
+    Update each dataset's html
+    """
+    # Open and read the HTML file
+    with open(_path, 'r') as file:
+        html_content = file.read()
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Change the main text
+    h2_tag_main_title = soup.find('h2', class_="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight")
+    h2_tag_main_title.string = '\n              Dataset'
+
+    # Modify the Catalogue home link
+    a_tag_top_left = soup.find('a', class_="text-xl text-gray-900 font-semibold")
+    a_tag_top_left.string = '\n              <- Back to Catalogue Home\n            '
+    a_tag_top_left['href'] = "https://un-task-team-for-scanner-data.github.io/price-stats-data-catalogue/"
+    a_tag_top_left
+
+    # Modify the reference to the project
+    a_tag_top_right = soup.find('a', class_="text-sky-500 hover:text-gray-700 text-sm font-semibold")
+    # <a class="text-sky-500 hover:text-gray-700 text-sm font-semibold" href="https://datacontract.com" target="_blank">datacontract.com</a>
+    a_tag_top_right.string = 'Price Statistics Reproducibility Project'
+    a_tag_top_right['href'] = "https://un-task-team-for-scanner-data.github.io/reproducibility-project/docs/"
+
+    # Save the modified HTML back to the file
+    with open(_path, 'w') as file:
+        file.write(str(soup))
+
 
 if __name__ == "__main__":
     update_index()
+    files = os.listdir('_site/')
+    files.remove('index.html')
+    for file in files:
+        update_dataset('_site/'+file)
